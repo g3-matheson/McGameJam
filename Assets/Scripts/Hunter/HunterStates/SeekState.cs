10c618815884d84c03716de416bf8569a66ccf72
Kat Matheson <g3.matheson@gmail.com>
2026-01-31 12:09:28 -0500

    GameManager init, EnemyAI stuff

---
+++ Assets/Scripts/Hunter/HunterStates/SeekState.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SeekState : HunterState
+{
+    public override void Enter(HunterAI ai)
+    {
+    }
+
+    public override void Exit(HunterAI ai)
+    {
+    }
+
+    public override void Tick(HunterAI ai)
+    {
+    }
+}
