2f9023036c73c90d2a8f7c5683cd74f34933b995
Kat Matheson <g3.matheson@gmail.com>
2026-02-01 09:19:35 -0500

    lock logic done

--- Assets/Scripts/LockInteraction.cs
+++ Assets/Scripts/LockInteraction.cs
@@ -1,35 +1,19 @@
 using UnityEngine;
-using TMPro;
-using System.Linq;
 
 public class LockInteraction : MonoBehaviour, Interactable
 {
-
-    public int[] numbers;
-    private int index = 0;
-
-    void Start()
-    {
-        
-    }
-
     public void Interact(PlayerController player)
     {
-        if (UIManager.instance.lockPad.gameObject.activeInHierarchy)
-            {
-                UIManager.instance.lockPad.gameObject.SetActive(true);
-            
-            if (player.bIsInteracting)
-            {
-                UIManager.instance.lockPad.CheckCombination();
-                player.bIsInteracting = false;
-            }
-        }
-        else
+        if (!UIManager.instance.lockPad.gameObject.activeInHierarchy)
         {
-            
             UIManager.instance.lockPad.gameObject.SetActive(true);
             player.bIsInteracting = true;
         }
+        else
+        {
+            UIManager.instance.lockPad.gameObject.SetActive(false);
+            player.bIsInteracting = false;
+        }
+            
     }
 }
--- Assets/Scripts/UILockScript.cs
+++ Assets/Scripts/UILockScript.cs
@@ -1,18 +1,19 @@
 using TMPro;
 using UnityEngine;
+using System.Text.RegularExpressions;
 
 public class UILockScript : MonoBehaviour
 {
     public GameObject currentButton;
+    private int currentButtonNumber;
     public int currentNumber;
-    public int[] combination = new int[4]; 
-    private int[] Answer = {1, 2, 3, 4};
-
+    public int[] combination = { 1, 1, 1, 1};
+    public int[] Answer = {1, 2, 3, 4};
 
     public void CheckCombination()
     {
         bool result = true;
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 4; i++)
         {
             if (combination[i] != Answer[i]) result = false;
         }
@@ -26,25 +27,16 @@ public class UILockScript : MonoBehaviour
     public void ButtonPress(GameObject self)
     {   
         currentButton = self;
-        currentNumber = int.Parse(currentButton.GetComponentInChildren<TextMeshProUGUI>().text);
-        currentButton.GetComponentInChildren<TextMeshProUGUI>().text = (currentNumber + 1).ToString();
-        for (int i = 0; i > 3; i++)
-        {
-            if (currentButton.name == $"Button {i}")
-            {
-                combination[i] = currentNumber;
-                break;
-            }
-        }
+        currentButtonNumber = int.Parse(currentButton.transform.name.Replace("Button",""));
         
-        if (currentNumber >= 9)
-        {
-            currentButton.GetComponentInChildren<TextMeshProUGUI>().text = "0";
-        }
+        combination[currentButtonNumber] = combination[currentButtonNumber] + 1;
+        if (combination[currentButtonNumber] == 10) combination[currentButtonNumber] = 1; 
 
+        currentButton.GetComponentInChildren<TextMeshProUGUI>().text = combination[currentButtonNumber].ToString();
+
+        Debug.Log($"Set Button {currentButtonNumber} to {combination[currentButtonNumber]}");
+        
         CheckCombination();
-
-
     }
 
 }
