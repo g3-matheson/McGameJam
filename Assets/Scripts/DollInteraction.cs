629b74a1f42c88ff3c5f9dc93723cde2207045f2
Miguel Chartrand <miguelchartrand@gmail.com>
2026-02-01 04:31:23 -0500

    Add Audio Source to Doll.

--- Assets/Scripts/DollInteraction.cs
+++ Assets/Scripts/DollInteraction.cs
@@ -3,6 +3,16 @@ using UnityEngine;
 public class DollInteraction : MonoBehaviour, Interactable
 {
     public string[] lines;
+
+    public AudioClip[] dollAudio;
+
+    public AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void Interact(PlayerController player)
     {
         
@@ -16,17 +26,21 @@ public class DollInteraction : MonoBehaviour, Interactable
             else if (UIManager.instance.dialogueBox.index >= lines.Length - 1)
             {
                 UIManager.instance.dialogueBox.NextLine();
+                audioSource.Stop();
                 player.bIsInteracting = false;
             }
             
             else
             {
                 UIManager.instance.dialogueBox.NextLine();
+                if(dollAudio.Length <=0 ) return;
+                audioSource.PlayOneShot(dollAudio[UIManager.instance.dialogueBox.index]);
             }
         }
         else
         {
             UIManager.instance.dialogueBox.SetLines(lines);
+            if(dollAudio.Length > 0){ audioSource.PlayOneShot(dollAudio[0]); }
             player.bIsInteracting = true;
         }
         
